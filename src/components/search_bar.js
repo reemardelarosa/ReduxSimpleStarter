1e3cf330811e2361800c62ad907f6a85d83db974
delaj161 <john.x.dela.-nd@disney.com>
2018-02-27 08:50:14 +0800

    Added youtube search react app

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,9 +8,19 @@ class SearchBar extends Component {
     }
 
     render() {
-        return <input onChange={
-            event => console.log(event.target.value)
-        }/>;
+        return (
+            <div className="search-bar">
+                <input 
+                    value={this.state.term}
+                    onChange={ event =>   this.onInputChange(event.target.value)}/>
+                Value of the input: { this.state.term }
+            </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({ term });
+        this.props.onSearchTermChange(term);
     }
 }
 
